759816f6d274463f6d8846537b7d52dd0924a039
web3santa <usefulstory35@gmail.com>
2024-04-11 18:31:52 +0900

    data model for order added

---
+++ model/order.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Order struct {
+	OrderID     uint64     `json:"order_id"`
+	CustomerID  uuid.UUID  `json:"customer_id"`
+	LineItems   []LineItem `json:"line_items"`
+	OrderStatus string     `json:"order_status"`
+	CreatedAt   *time.Time `json:"created_at"`
+	ShippedAt   *time.Time `json:"shipped_at"`
+	CompletedAt *time.Time `json:"completed_at"`
+}
+
+type LineItem struct {
+	ItemID   uuid.UUID `json:"item_id"`
+	Quantity uint      `json:"quantity"`
+	Price    uint      `json:"price"`
+}
